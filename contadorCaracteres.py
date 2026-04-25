74c323183459e4f07ce1aa9957a6e2bf1b1513b4
GabitoGabincho <gabriel.dellavechia@hotmail.com>
2026-04-25 20:39:17 -0300

    codigo modificado

--- contadorCaracteres.py
+++ contadorCaracteres.py
@@ -1,8 +1,16 @@
-palabra = input("Ingresar un text o palabra:")
+palabra = input("Ingresar un text o palabra:").strip()
 cantidadcaracteres = len(palabra)
+
+#convertir el testo en Mayuscula 
 
 if cantidadcaracteres == 0:
     print("No se ingreso palabra o text")
-else: 
-    print(f'la cantidad de palabras que contiene es de {cantidadcaracteres} y el texto o palabra ingresado fue {palabra}')
-    
+elif palabra.isupper(): 
+     pm = palabra.lower()
+     print(f'la cantidad de palabras que contiene es de {cantidadcaracteres}  y el texto en minuscula ingresado fue {pm}')
+else:
+    if palabra.islower():
+        pmm = palabra.upper()
+        print(f'la cantidad de palabras que contiene es de {cantidadcaracteres}  y el texto en mayuscula ingresado fue {pmm}')
+    else:
+        print(f'la cantidad de palabras que contiene es de {cantidadcaracteres}  y el texto ingresado NO se modifica   {palabra}')
