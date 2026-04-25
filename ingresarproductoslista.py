efecfc918fa5aa972fe777b06a9aa49646e80d65
GabitoGabincho <gabriel.dellavechia@hotmail.com>
2026-04-25 20:52:51 -0300

    codigo modificado2

---
+++ ingresarproductoslista.py
@@ -0,0 +1,13 @@
+lista = []
+valor1 = input("Ingresar Producto #1 : ")
+lista.append(valor1)
+valor2 = input("Ingresar Producto #2 : ")
+lista.append(valor2)
+valor3 = input("Ingresar Producto #3 : ")
+lista.append(valor3)
+valor4 = input("Ingresar Producto #4 : ")
+lista.append(valor4)
+valor5 = input("Ingresar Producto #5 : ")
+lista.append(valor5)
+
+print(lista)
